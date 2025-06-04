997791c496c79cd68cfd0d78396080bbaee58075
Dennis Riis <dr@driis.dk>
2025-06-04 21:46:39 +0200

    Simplistic test

---
+++ dr.RpiClock.Tests/IntegrationTest.cs
@@ -0,0 +1,21 @@
+﻿using dr.RpiClock.App;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace dr.RpiClock.Tests;
+
+[TestFixture]
+public class IntegrationTest
+{
+    [Test]
+    public async Task CanRenderOutputToBmp()
+    {
+        var outFile = Path.Combine(Path.GetTempPath(), Path.ChangeExtension(Path.GetRandomFileName(), "bmp"));
+        var config = Configuration.CreateServiceProvider([outFile]);
+        var renderer = config.GetRequiredService<RenderService>();
+        
+        await renderer.Run(default);
+        
+        Assert.That(File.Exists(outFile));
+        Console.WriteLine($"Rendered output image to file://{outFile}");
+    }
+}
